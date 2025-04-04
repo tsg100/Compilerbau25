ae01e4da24c02b04274c2b89e9d520058dbb8222
Fynn Henck <fynn.henck@sap.com>
2025-04-04 15:49:56 +0200

    Added LineCommentMachine

---
+++ src/main/java/com/compiler/machines/LineCommentMachine.java
@@ -0,0 +1,43 @@
+package com.compiler.machines;
+
+import com.compiler.State;
+import com.compiler.StateMachine;
+import com.compiler.TokenIntf.Type;
+
+public class LineCommentMachine extends StateMachine {
+
+    @Override
+    public void initStateTable() {
+        State firstCommentSlashState = new State("firstSlash", false);
+        firstCommentSlashState.addTransition('/', "secondSlash");
+        addState(firstCommentSlashState);
+
+        State secondCommentSlashState = new State("secondSlash", false);
+        secondCommentSlashState.addTransition('/', "comment");
+        addState(secondCommentSlashState);
+
+        //we are now in the comment - allow any character
+        //also allow whitespace after //
+        //go over to end state when \n is read
+        State commentState = new State("comment", false);
+        commentState.addTransitionRange(' ', '~', "comment");
+        //german special characters (e.g. ä, ö, ü, ...) are excluded (because im too lazy) D:<
+
+        commentState.addTransition('\n', "end");
+        addState(commentState);
+
+        State endState = new State("end", true);
+        addState(endState);
+
+    }
+
+    @Override
+    public String getStartState() {
+        return "firstSlash";
+    }
+
+    @Override
+    public Type getType() {
+        return Type.STRING;
+    }
+}
