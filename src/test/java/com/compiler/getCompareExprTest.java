ece434aecd050798842405ebd40db86f7c0d7841
Fynn Henck <fynn.henck@sap.com>
2025-04-25 07:34:21 +0200

    Fixed import issue (again)

--- src/test/java/com/compiler/getCompareExprTest.java
+++ src/test/java/com/compiler/getCompareExprTest.java
@@ -2,6 +2,7 @@ package com.compiler;
 
 import static org.junit.Assert.assertEquals;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 public class getCompareExprTest extends TestExpressionEvaluatorBase{
@@ -9,16 +10,16 @@ public class getCompareExprTest extends TestExpressionEvaluatorBase{
     @Test
     public void testCompareExprTest() throws Exception {
         // Check Java Compiler vs. unser Compiler
-        assertEquals(1, evalExpression("4>1"));
-        assertEquals(0, evalExpression("4<1"));
+        Assertions.assertEquals(1, evalExpression("4>1"));
+        Assertions.assertEquals(0, evalExpression("4<1"));
 
         // Check Ergebnis vs. unser Compiler
-        assertEquals(0, evalExpression("5==2"));
-        assertEquals(1, evalExpression("2==2"));
+        Assertions.assertEquals(0, evalExpression("5==2"));
+        Assertions.assertEquals(1, evalExpression("2==2"));
 
         // Komplexere Ausdrücke
-        assertEquals(1, evalExpression("1>0=0"));
-        assertEquals(0, evalExpression("1<0=0"));
+        Assertions.assertEquals(1, evalExpression("1>0=0"));
+        Assertions.assertEquals(0, evalExpression("1<0=0"));
     }
 
 }
