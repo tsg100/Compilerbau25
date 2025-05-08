ff0d109b4319dbf7e06e4aecb10f29188609cbed
fxnnhx <155892725+fxnnhx@users.noreply.github.com>
2025-05-08 11:54:04 +0200

    add stmtParser GroupA tests

--- src/test/java/com/compiler/StmtParserTest.java
+++ src/test/java/com/compiler/StmtParserTest.java
@@ -11,4 +11,18 @@ public class StmtParserTest extends StmtParserTestBase
                 """;
         // testParser(program, "<result>");
     }
+
+    @Test
+    public void testParserGroupA() throws Exception {
+        // com.compiler.Lexer lexer = new com.compiler.Lexer();
+        // com.compiler.StmtParser parser = new com.compiler.StmtParser(lexer);
+        // String program = """
+        //         {
+        //         PRINT 0 ? 2: 1;
+        //         PRINT 2 ? 2: 1;
+        //         }
+        //         """;
+        // String expOutput = "1\n2\n";
+        // testParser(program, expOutput);
+    }
 }
