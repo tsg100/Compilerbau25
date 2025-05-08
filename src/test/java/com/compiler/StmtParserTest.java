64d8bd708e5d60525d9fb0d328e20ec3312422f6
Eble <markus.eble@sap.com>
2025-05-08 17:04:17 +0200

    StmtParserTest

--- src/test/java/com/compiler/StmtParserTest.java
+++ src/test/java/com/compiler/StmtParserTest.java
@@ -8,7 +8,16 @@ public class StmtParserTest extends StmtParserTestBase
     public void testParser() throws Exception
     {
         String program = """
+        {
+          DECLARE a;
+          DECLARE b;
+          a = 1 + 2;
+          b = 5;
+          PRINT a ? b + 1 : -1;
+          PRINT 1 + 2;
+          PRINT 3 + 4;
+        }
                 """;
-        // testParser(program, "<result>");
+        testParser(program, "6\n3\n7\n");
     }
 }
