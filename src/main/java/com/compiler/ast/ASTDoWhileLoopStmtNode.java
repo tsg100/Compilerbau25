0c1bd3f7b4496e232099fe70b71c95dad5c44a36
Gilbert Becker <gilbertbckrgithub@gmail.com>
2025-05-23 12:22:01 +0200

    added codegen for (do) while

--- src/main/java/com/compiler/ast/ASTDoWhileLoopStmtNode.java
+++ src/main/java/com/compiler/ast/ASTDoWhileLoopStmtNode.java
@@ -2,6 +2,11 @@ package com.compiler.ast;
 
 import java.io.OutputStreamWriter;
 
+import com.compiler.CompileEnvIntf;
+import com.compiler.InstrBlock;
+import com.compiler.instr.InstrCondJump;
+import com.compiler.instr.InstrJump;
+
 public class ASTDoWhileLoopStmtNode extends ASTStmtNode {
 
     ASTExprNode m_predicate;
@@ -25,5 +30,25 @@ public class ASTDoWhileLoopStmtNode extends ASTStmtNode {
         outStream.write(indent + "DO WHILE\n");
         m_predicate.print(outStream, "  " + indent);
         m_loopBody.print(outStream, "  " + indent);
+    }
+
+    @Override
+    public void codegen(CompileEnvIntf env) {
+        InstrBlock whileHead = env.createBlock("DoWhileLoopHead");
+        InstrBlock whileBody = env.createBlock("DoWhileLoopBody");
+        InstrBlock exit = env.createBlock("DoWhileExitBlock");
+        InstrJump jumpToBody = new InstrJump(whileBody);
+        env.addInstr(jumpToBody);
+
+        env.setCurrentBlock(whileBody);
+        m_loopBody.codegen(env);
+        whileBody.addInstr(new InstrJump(whileHead));
+
+        env.setCurrentBlock(whileHead);
+        InstrCondJump conditionalJump = new InstrCondJump(m_predicate.codegen(env), whileBody, exit);
+
+        whileHead.addInstr(conditionalJump);
+
+        env.setCurrentBlock(exit);
     }
 }
